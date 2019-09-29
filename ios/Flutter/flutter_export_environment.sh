166e526596156147eae2cf39f3c1c444ae7c0c2c
Spawndemonic <spawndemonic@gmail.com>
2019-09-29 02:38:52 -0400

    inital

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/black/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/black/Documents/Projects/dnews"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/black/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
---
+++ lib/screens/main_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('DNews')
+      ),
+       
+    );
+    
+  }
+  
+  Widget _buildRow(){
+//Build Rows
+  //Each Row will contain an artticle title
+  //when clicked opens article(read in app or in browser(pub package: url_launcher)?)
+  }
+  
+}
