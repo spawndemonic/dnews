40bad81aaa2a60cdc8b4bc5255ae7f8edddf7a3d
Spawndemonic <spawndemonic@gmail.com>
2019-11-03 13:49:43 -0500

    mismatch types

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:webfeed/domain/rss_feed.dart';
 import "../feed.dart";
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MainPageState();
+  }
+}
+
+class _MainPageState extends State<MainPage> {
+  List<dynamic> _feed = articles();
   Widget build(BuildContext context) {
-    final sources = articles();
     return Scaffold(
       appBar: AppBar(
         title: Text('DNews'),
       ),
-      body: Card(
-          child: Column(
-        children: <Widget>[
-          
+      body: Column(
+        children: [
+          Column(
+            children: _feed
+                .map((element) => Card(
+                      child: Column(
+                        children: <Widget>[
+                          Text(element.title),
+                        ],
+                      ),
+                    ))
+                .toList(),
+          ),
         ],
-      )),
+      ),
     );
   }
 }
