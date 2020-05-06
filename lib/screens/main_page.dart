eead3cc81b150e3a090fd177f37f14664071ad9c
black <black@localhost.localdomain>
2020-05-06 12:55:52 -0400

    some changes redoing the whole thing might as well

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -8,49 +8,39 @@ import 'dart:io';
  
 class MainPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MainPageState();
-  }
+  State<StatefulWidget> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
-  List _feed;
+  List<RssItem> _feed;
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('DNews'),
       ),
-      body: Column(
-        children: [
-          Column(
-            children: _feed
-                .map((element) => Card(
-                      child: Column(
-                        children: <Widget>[
-                          Text(element.title),
-                        ],
-                      ),
-                    ))
-                .toList(),
-          ),
-        ],
+      body: FutureBuilder(
+        future:articles(),
+      
       ),
     );
   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
    articles() async{
      var client = new http.Client();
-    //var file= File('source.txt');
-    //var contents;
-    //if(await file.exists()){
-    //  contents = await file.readAsString();
-    //}
-  // RSS feed
-  client.get("http://rss.nytimes.com/services/xml/rss/nyt/World.xml").then((response) {
+  return client.get("http://rss.nytimes.com/services/xml/rss/nyt/World.xml").then((response) {
     return response.body;
   }).then((bodyString) {
     var channel = new RssFeed.parse(bodyString);
-    _feed = channel.items.toList();
+    return channel.items.toList();
   });
   }
 }
